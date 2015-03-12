4f1d00f98f9418bdbb034ec26a0503cb695a4f3e
wolodarsky <tysonsafavi@me.com>
2015-03-11 22:06:31 -0700

    csrf controller tests

--- test/controllers/csrf_controller_test.rb
+++ test/controllers/csrf_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class CsrfControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should return csrf token" do
+    get :index
+    assert_response :success
+    assert(JSON.parse(@response.body).has_key?('authenticity_token'))
+  end
 end
