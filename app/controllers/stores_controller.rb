f5035b4815072bedd96c351ad841459fe73414b9
wolodarsky <tysonsafavi@me.com>
2015-04-05 00:06:17 -0700

    moved token auth to application controller

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -77,14 +77,4 @@ class StoresController < ApplicationController
       params.require(:store).permit(:name, :address_1, :address_2, :city, :state, :zipcode, :phone, :url)
     end
 
-    def authenticate_user_from_token!
-      authenticate_with_http_token do |token, options|
-        user_email = options[:email].presence
-        user = user_email && User.find_by_email(user_email)
-
-        if user && Devise.secure_compare(user.authentication_token, token)
-          sign_in user, store: false
-        end
-      end
-    end
 end
