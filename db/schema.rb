6808ff1ad60f23dbf839568f97788943e7edc61d
wolodarsky <tysonsafavi@me.com>
2015-04-08 22:59:14 -0700

    added confirmable to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150405000711) do
+ActiveRecord::Schema.define(version: 20150408062026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,10 @@ ActiveRecord::Schema.define(version: 20150405000711) do
     t.datetime "updated_at"
     t.string   "authentication_token"
     t.boolean  "admin",                  default: false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
