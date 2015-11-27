f8e4fcac77488412a5b83bc5b4f0f7a7b87f7cd0
wolodarsky <tysonsafavi@me.com>
2015-11-26 22:40:37 -0700

    forwarding to squarespace site

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,10 +1,11 @@
 class IndexController < ApplicationController
   def index
-    manifest_id = params[:manifest_id]
-    if manifest_id.blank?
-      manifest_id = REDIS.get("ssj-ember:current")
-    end
-    html = REDIS.get(manifest_id)
-    render :text => html
+    redirect_to("https://sarahsafavijewelry.squarespace.com")
+    #manifest_id = params[:manifest_id]
+    #if manifest_id.blank?
+      #manifest_id = REDIS.get("ssj-ember:current")
+    #end
+    #html = REDIS.get(manifest_id)
+    #render :text => html
   end
 end
